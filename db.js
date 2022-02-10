0928c7923b7325b77ac792c6c30e9baa993e6801
Percell Gaskins <pmg8899@gmail.com>
2022-02-10 13:59:55 -0800

    created GET/

---
+++ db.js
@@ -0,0 +1,8 @@
+const destinations = [
+  {
+    destination: "Eiffel Tower",
+    location: "Paris, France",
+  },
+];
+// export
+module.exports = { destinations };
---
+++ index.js
@@ -0,0 +1,20 @@
+const { destinations } = require("./db");
+//server ask require express
+const express = required(express);
+//express lib
+const res = require("express/lib/response");
+const { send } = require("express/lib/response");
+
+//PORT Variable
+const PORT = 3000;
+//Creates a express server
+const server = express();
+//use listen method()with port
+server.listen(PORT, () => {
+  console.log("Server listening on Port 3000");
+});
+
+//Endpoints (Routes) are made up of the method and the path
+server.get("/", (res, req) => {
+  res.send(destionations);
+});
