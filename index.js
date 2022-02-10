63e22f64e5b93506c1cf5412f9bde6fcbdd6da7a
Percell Gaskins <pmg8899@gmail.com>
2022-02-10 15:28:59 -0800

    "push evt"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { destinations } = require("./db");
 //server ask require express
 const express = require("express");
+const cors = require("cors");
 //express lib
 
 //PORT Variable
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 //Creates a express server sit
 
 const server = express();
+
+server.use(cors());
 //use listen method()with port
 server.listen(PORT, () => {
   console.log("Server listening on Port 3000");
